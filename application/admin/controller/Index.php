823240e5b6760378310575c4a4f41d7fc952efdf
zhouzj <zhouzj@idoosmart.com>
2018-05-29 12:30:07 +0800

    tst

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2018/5/29
+ * Time: 9:20
+ */
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function test(){
+        return $this->fetch();
+    }
+
+    public function welcome(){
+        return "welcome zzj";
+    }
+}
